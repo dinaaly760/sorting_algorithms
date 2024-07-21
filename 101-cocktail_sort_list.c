39580de60153fde43e220ad1a018732f76a1fb64
Dina Aly <dinaaly760@gmail.com>
2024-07-21 01:00:33 -0700

    sorting

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-
+#include <stdbool.h>
 void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
 void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
 void cocktail_sort_list(listint_t **list);
@@ -64,7 +64,7 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 void cocktail_sort_list(listint_t **list)
 {
 	listint_t *tail, *shaker;
-	bool shaken_not_stirred = false;
+	bool shaken_not_stirred = 0;
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
@@ -72,16 +72,16 @@ void cocktail_sort_list(listint_t **list)
 	for (tail = *list; tail->next != NULL;)
 		tail = tail->next;
 
-	while (shaken_not_stirred == false)
+	while (shaken_not_stirred == 0)
 	{
-		shaken_not_stirred = true;
+		shaken_not_stirred = 1;
 		for (shaker = *list; shaker != tail; shaker = shaker->next)
 		{
 			if (shaker->n > shaker->next->n)
 			{
 				swap_node_ahead(list, &tail, &shaker);
 				print_list((const listint_t *)*list);
-				shaken_not_stirred = false;
+				shaken_not_stirred = 0;
 			}
 		}
 		for (shaker = shaker->prev; shaker != *list;
@@ -91,7 +91,7 @@ void cocktail_sort_list(listint_t **list)
 			{
 				swap_node_behind(list, &tail, &shaker);
 				print_list((const listint_t *)*list);
-				shaken_not_stirred = false;
+				shaken_not_stirred = 0;
 			}
 		}
 	}
